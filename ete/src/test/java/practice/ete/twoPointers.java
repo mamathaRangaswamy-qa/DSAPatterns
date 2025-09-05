8253cad9017cb2cbe5f88ccdbb284aa43bcd00de
mamatha.r <mamatha.r@BLR1-LHP-N93294.synapse.com>
2025-09-06 04:03:07 +0530

    Initial commit

---
+++ ete/src/test/java/practice/ete/twoPointers.java
@@ -0,0 +1,10 @@
+package practice.ete;
+
+public class twoPointers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("start");
+	}
+
+}
